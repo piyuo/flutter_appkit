db6deb4e06080b495022e3d57e0575d25bef311f
piyuo <piyuo.com@gmail.com>
2020-03-11 12:56:15 -0700

    remove log

--- lib/i18n/i18n_state.dart
+++ lib/i18n/i18n_state.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:libcli/hook/assets.dart' as assets;
-import 'package:libcli/log/log.dart';
 
 class I18nState {
   final Map<String, dynamic> _localization;
@@ -19,7 +18,6 @@ class I18nState {
 
 Future<I18nState> readState(String key) async {
   assert(key.length > 0);
-  'i18n_state|read "i18n/${key}.json"'.print;
   String jsonContent = await assets.get('i18n/${key}.json');
   var localization = json.decode(jsonContent);
   return I18nState(localization);
