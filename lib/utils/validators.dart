16575402747ba2ffef1769083958ddc05d6e0bbf
piyuo <piyuo.com@gmail.com>
2020-04-04 12:10:58 -0700

    refactor validator

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -2,46 +2,97 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:libcli/i18n/i18n.dart';
 
+/// requiredValidator validate input string, return error message when input is empty, other return null
+///
+///  use title to set error message title
+///
+///     String error = requiredValidator(context, nameField.value,''title);
+///
+String requiredValidator(BuildContext context, String input, String title) {
+  return input.length > 0
+      ? null
+      : '@required'.i18n(context).replaceAll('%1', title);
+}
+
+/// regexpValidator validate input string using regex, return error message when input not valid, other return null
+///
+///  use  title and example to set proper error message
+///
+///     RegExp regexp = RegExp(r"^[A-Za-z]");
+///     String error = regexpValidator(context, nameField.value, regexp, 'title', 'A-z')
+///
 String regexpValidator(
   BuildContext context,
-  String regex,
   String input,
-  String name,
+  RegExp regexp,
+  String title,
   String example,
 ) {
-  final regexp = new RegExp(
-    regex,
-    caseSensitive: false,
-    multiLine: false,
-  );
-
   return regexp.hasMatch(input)
       ? null
       : '@validator'
           .i18n(context)
-          .replaceAll('%1', name)
+          .replaceAll('%1', title)
           .replaceAll('%2', example);
 }
 
+/// emailRegexp regexp use to validate email
+///
+RegExp emailRegexp() {
+  return RegExp(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)");
+}
+
+/// emailValidator validate input string is email, return error message when input not valid, other return null
+///
+///     String error = emailValidator(context, 'johndoe@domain.com');
+///
 String emailValidator(BuildContext context, String input) {
   return regexpValidator(
-      context,
-      r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
-      input,
-      '@email'.i18n(context),
-      'johndoe@domain.com');
+    context,
+    input,
+    emailRegexp(),
+    '@email'.i18n(context),
+    'johndoe@domain.com',
+  );
 }
 
+/// domainNameRegexp regexp use to validate domain name
+///
+RegExp domainNameRegexp() {
+  return RegExp(
+      r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]");
+}
+
+/// domainNameValidator validate input string is domain name, return error message when input not valid, other return null
+///
+///     String error = domainNameValidator(context, 'johndoe@domain.com');
+///
 String domainNameValidator(BuildContext context, String input) {
   return regexpValidator(
-      context,
-      r"(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]",
-      input,
-      '@domain'.i18n(context),
-      'www.domain.com');
+    context,
+    input,
+    domainNameRegexp(),
+    '@domain'.i18n(context),
+    'www.domain.com',
+  );
 }
 
+/// urlRegexp regexp use to validate url
+///
+RegExp urlRegexp() {
+  return RegExp(r"^(https?)://[^\s/$.?#].[^\s]*$");
+}
+
+/// urlValidator validate input string is url, return error message when input not valid, other return null
+///
+///     String error = urlValidator(context, 'http://www.g.com');
+///
 String urlValidator(BuildContext context, String input) {
-  return regexpValidator(context, r"^(https?)://[^\s/$.?#].[^\s]*$", input,
-      '@url'.i18n(context), 'http://www.domain.com');
+  return regexpValidator(
+    context,
+    input,
+    urlRegexp(),
+    '@url'.i18n(context),
+    'http://www.domain.com',
+  );
 }
--- test/utils/validators_test.dart
+++ test/utils/validators_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:libcli/utils/validators.dart';
 
@@ -6,6 +5,21 @@ void main() {
   setUp(() async {});
 
   group('[validators]', () {
+    test('should validate string is empty', () async {
+      String error = requiredValidator(null, '', 'title');
+      expect(error, isNotNull);
+      error = requiredValidator(null, 'has value', 'title');
+      expect(error, isNull);
+    });
+
+    test('should validate using regex', () async {
+      RegExp regexp = RegExp(r"^[A-Za-z]");
+      String error = regexpValidator(null, '1', regexp, 'title', 'A-z');
+      expect(error, isNotNull);
+      error = regexpValidator(null, 'A', regexp, 'title', 'A-z');
+      expect(error, isNull);
+    });
+
     test('should validate email', () async {
       String error = emailValidator(null, '1');
       expect(error, isNotNull);
@@ -16,7 +30,14 @@ void main() {
     test('should validate domain name', () async {
       String error = domainNameValidator(null, 'a');
       expect(error, isNotNull);
-      error = domainNameValidator(null, 'www.gmail.com');
+      error = domainNameValidator(null, 'www.g.com');
+      expect(error, isNull);
+    });
+
+    test('should validate url', () async {
+      String error = urlValidator(null, 'www.g.com');
+      expect(error, isNotNull);
+      error = urlValidator(null, 'http://www.ggg.com');
       expect(error, isNull);
     });
   });
