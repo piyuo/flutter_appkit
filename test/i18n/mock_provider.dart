e75e324326d7c2491c561a0a3123b8b98e7c34a6
piyuo <piyuo.com@gmail.com>
2020-04-05 22:09:17 -0700

    support load with buildcontext

--- test/i18n/mock_provider.dart
+++ test/i18n/mock_provider.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'package:flutter/material.dart';
 import 'package:libcli/pattern/async_provider.dart';
 
 class MockProvider extends AsyncProvider {
   @override
-  Future<void> load() async {
+  Future<void> load(BuildContext context) async {
     await Future.delayed(Duration(milliseconds: 1), () {});
   }
 }
--- test/pattern/mock_provider.dart
+++ test/pattern/mock_provider.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
 import 'package:libcli/pattern/async_provider.dart';
+import 'package:flutter/material.dart';
 
 class MockProvider extends AsyncProvider {
   @override
-  Future<void> load() async {
+  Future<void> load(BuildContext context) async {
     await Future.delayed(Duration(milliseconds: 1), () {});
   }
 }
