5aba382ef6ea21cb31a303ea4da0bb79a62ccc12
piyuo <piyuo.com@gmail.com>
2020-04-11 14:23:37 -0700

    refactor

--- lib/src/command_type/shared/ping_action.pb.dart
+++ lib/src/command_type/shared/ping_action.pb.dart
@@ -6,11 +6,11 @@
 // ignore_for_file: camel_case_types,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
 
 import 'dart:core' as $core;
-import 'package:libcli/src/command/command.dart' as command;
+import 'package:libcli/command.dart';
 
 import 'package:protobuf/protobuf.dart' as $pb;
 
-class PingAction extends command.ProtoObject {
+class PingAction extends ProtoObject {
   $core.int mapIdXXX() {
     return 4;
   }
