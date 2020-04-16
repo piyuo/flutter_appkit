4be96e71c5162f9c38f07f75115ae680cd7ea095
piyuo <piyuo.com@gmail.com>
2020-04-15 23:22:41 -0700

    page name can be empty but not null

--- lib/src/i18n/i18n_provider.dart
+++ lib/src/i18n/i18n_provider.dart
@@ -12,10 +12,10 @@ class I18nProvider extends ReduxProvider<I18nState, dynamic> {
 
   @override
   Future<void> load(BuildContext context) async {
-    assert(_pageName.length > 0, 'need page name');
+    assert(_pageName != null, 'need page name');
     assert(i18n.locale != null, "need I18nDelegate to localizationsDelegates");
-    state =
-        await readPageTranslation(_pageName ?? '', i18n.languageCode, i18n.countryCode);
+    state = await readPageTranslation(
+        _pageName ?? '', i18n.languageCode, i18n.countryCode);
   }
 
   String translate(String key) {
