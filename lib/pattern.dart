903c7db286c3ce86390aaac209c8a8630de76dda
piyuo <piyuo.com@gmail.com>
2020-04-17 09:00:10 -0700

    export provider_widget

--- lib/pattern.dart
+++ lib/pattern.dart
@@ -5,3 +5,4 @@ export 'package:libcli/src/pattern/history_redux.dart';
 export 'package:libcli/src/pattern/redux_provider.dart';
 export 'package:libcli/src/pattern/redux.dart';
 export 'package:libcli/src/pattern/await.dart';
+export 'package:libcli/src/pattern/provider_widget.dart';
