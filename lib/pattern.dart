b16f20bee3e8364b7196aff4efc92b68e40db5ef
piyuo <piyuo.com@gmail.com>
2020-04-22 21:55:07 -0700

    export contracts

--- lib/pattern.dart
+++ lib/pattern.dart
@@ -6,3 +6,4 @@ export 'package:libcli/src/pattern/redux_provider.dart';
 export 'package:libcli/src/pattern/redux.dart';
 export 'package:libcli/src/pattern/await.dart';
 export 'package:libcli/src/pattern/provider_widget.dart';
+export 'package:libcli/src/pattern/contracts.dart';
