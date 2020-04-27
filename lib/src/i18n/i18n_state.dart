ba9b1945beb68aa31ae309986ec249db19fbe735
piyuo <piyuo.com@gmail.com>
2020-04-26 22:59:43 -0700

    add global translation

--- lib/src/i18n/i18n_state.dart
+++ lib/src/i18n/i18n_state.dart
@@ -3,7 +3,11 @@ import 'dart:convert';
 import 'package:libcli/asset.dart' as asset;
 import 'package:libcli/src/i18n/i18n.dart' as i18n;
 
-Map<String, dynamic> _libLocalization = null;
+Map<String, dynamic> _globalLocalization = null;
+
+String global(String key) {
+  return _globalLocalization[key];
+}
 
 class I18nState {
   final Map<String, dynamic> _localization;
@@ -11,7 +15,11 @@ class I18nState {
   I18nState(this._localization);
 
   String translate(String key) {
-    return _localization[key];
+    var result = _localization[key];
+    if (result == null) {
+      return global(key);
+    }
+    return result;
   }
 
   Map toJson() {
@@ -25,33 +33,31 @@ Future<Map<String, dynamic>> getTranslationFromLib(
   String languageCode,
   String countryCode,
 ) async {
-  if (_libLocalization == null) {
+  if (_globalLocalization == null) {
     String libJson = await asset.loadJson(
         'i18n/$languageCode/$countryCode/lib.json',
         package: 'libcli');
-    _libLocalization = json.decode(libJson);
+    _globalLocalization = json.decode(libJson);
   }
-  return _libLocalization;
+  return _globalLocalization;
 }
 
 /// getTranslationFromAsset load translation from assets/i18n
 ///
 Future<I18nState> getTranslationFromAsset(
-  String filename,
-  String languageCode,
-  String countryCode,
-) async {
+    String filename, String languageCode, String countryCode) async {
   assert(filename != null);
-  Map<String, dynamic> localization;
+  if (_globalLocalization == null) {
+    _globalLocalization =
+        await getTranslationFromLib(languageCode, countryCode);
+  }
 
+  var localization = Map<String, dynamic>();
   if (filename.isNotEmpty) {
     String pageJson = await asset
         .loadJson('i18n/$languageCode/$countryCode/${filename}.json');
     localization = json.decode(pageJson);
-  } else {
-    localization = Map<String, dynamic>();
   }
-  localization.addAll(await getTranslationFromLib(languageCode, countryCode));
   return I18nState(localization);
 }
 
--- test/i18n/i18n_state_test.dart
+++ test/i18n/i18n_state_test.dart
@@ -1,27 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:libcli/i18n.dart';
+import 'package:libcli/i18n.dart' as i18n;
 import 'package:flutter/material.dart';
 
 void main() {
-  mockI18n(Locale('en', 'US'), '{"a": "A"}');
+  i18n.mockI18n(Locale('en', 'US'), '{"a": "A"}');
 
   setUp(() async {});
 
   group('[i18n_state]', () {
     test('should getTranslationFromLib', () async {
-      var translation = await getTranslationFromLib('en', 'US');
+      var translation = await i18n.getTranslationFromLib('en', 'US');
       expect(translation, isNotNull);
       expect(translation['a'], 'A');
     });
 
     test('should getTranslationFromAsset', () async {
-      var translation = await getTranslationFromAsset('any', 'en', 'US');
+      var translation = await i18n.getTranslationFromAsset('any', 'en', 'US');
       expect(translation, isNotNull);
       expect(translation.translate('a'), 'A');
+      expect(i18n.global('a'), 'A');
     });
 
     test('should getTranslation', () async {
-      var translation = await getTranslation('any');
+      var translation = await i18n.getTranslation('any');
       expect(translation, isNotNull);
       expect(translation.translate('a'), 'A');
     });
