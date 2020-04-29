8b14486e6bcf60717354ad7ca05539483def349e
piyuo <piyuo.com@gmail.com>
2020-04-29 12:08:04 -0700

    add data

---
+++ lib/data.dart
@@ -0,0 +1,4 @@
+library data;
+
+export 'package:libcli/src/data/countries.dart';
+export 'package:libcli/src/data/languages.dart';
---
+++ lib/src/data/languages.dart
@@ -0,0 +1,13 @@
+List languages() {
+  return [
+    ["Chinese (中文)", "zh"],
+    ["English", "en"],
+    ["French (Français)", "fr"],
+    ["German (Deutsch)", "de"],
+    ["Italian (Italiano)", "it"],
+    ["Japanese (日本語)", "jp"],
+    ["Korean (한국어)", "ko"],
+    ["Spanish (Español)", "es"],
+    ["Swedish (Svenska)", "sv"],
+  ];
+}
