b0800125b57fb614bec0bc823d7ee94af2147780
piyuo <piyuo.com@gmail.com>
2020-04-29 17:45:44 -0700

    rename libcliPlaygrounds

--- lib/playgrounds.dart
+++ lib/playgrounds.dart
@@ -1,4 +1,4 @@
-library playgrounds;
+library libcliPlaygrounds;
 
 export 'package:libcli/src/dialog/playground/dialog_playground.dart';
 export 'package:libcli/src/client/playground/client_playground.dart';
