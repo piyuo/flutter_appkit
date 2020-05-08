d8772acfc0380b2fa9cda4effc9c598a44b5dee3
piyuo <piyuo.com@gmail.com>
2020-05-07 17:38:20 -0700

    add regext text input formatter

---
+++ lib/src/validator/regexp_text_input_formatter.dart
@@ -0,0 +1,25 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class RegexpTextInputFormatter extends TextInputFormatter {
+  /// A [regexp] to be match
+  ///
+  final RegExp regexp;
+
+  /// Creates a formatter that allows only the insertion of regexp match
+  ///
+  /// The [regexp] must not be null.
+  RegexpTextInputFormatter(this.regexp) : assert(regexp != null);
+
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue, // unused.
+    TextEditingValue newValue,
+  ) {
+    if (regexp.hasMatch(newValue.text)) {
+      return newValue;
+    }
+    return oldValue;
+  }
+}
--- lib/validator.dart
+++ lib/validator.dart
@@ -3,3 +3,4 @@ library validator;
 export 'package:libcli/src/validator/input_formatters.dart';
 export 'package:libcli/src/validator/mail_checker.dart';
 export 'package:libcli/src/validator/validators.dart';
+export 'package:libcli/src/validator/regexp_text_input_formatter.dart';
