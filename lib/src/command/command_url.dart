da64731146614171db88f0057508d452ef84644e
piyuo <piyuo.com@gmail.com>
2020-06-07 14:38:23 -0700

    update service url

--- lib/src/command/command_url.dart
+++ lib/src/command/command_url.dart
@@ -4,33 +4,33 @@ import 'package:libcli/i18n.dart' as i18n;
 ///
 String baseDomain = 'piyuo.com';
 
-/// serviceRegion is country where service locate
+/// serviceRegion is set if service need tie to specify region, usually when user sign in and they need to to same region where they create the account
 ///
 String serviceRegion;
 
 /// serviceBranch is service branch
 ///
-String serviceBranch = BRANCH_MASTER;
+String serviceBranch = BRANCH_STABLE;
 
-/// BRANCH_BETA is for development
+/// BRANCH_MASTER is The current tip-of-tree, absolute latest cutting edge build. Usually functional, though sometimes we accidentally break things
 ///
-const BRANCH_BETA = 'b';
+const BRANCH_MASTER = 'master';
 
-/// BRANCH_MASTER is for production
+/// BRANCH_BETA We will branch from master for a new beta release at the beginning of the month, usually the first Monday
 ///
-const BRANCH_MASTER = 'm';
+const BRANCH_BETA = 'beta';
+
+/// BRANCH_STABLE is a a branch that has been stabilized on beta will become our next stable branch and we will create a stable release from that branch. We recommend that you use this channel for all production app releases.
+///
+const BRANCH_STABLE = 'stable';
 
 /// serviceUrl return service url base on app.branch
 ///
-///     String url = serviceUrl('sys');
+///     String url = serviceUrl('sys'); // https://auth-us-stable.piyuo.com
 ///
 String serviceUrl(String funcName) {
-  String subdomain = 'api';
-  if (serviceBranch == BRANCH_BETA) {
-    subdomain = 'beta';
-  }
   // add /?q query string to avoid cache by cloud flare
-  return 'https://$funcName-${determineRegion()}.$subdomain.$baseDomain/?q';
+  return 'https://$funcName-${determineRegion()}-$serviceBranch.$baseDomain/?q';
 }
 
 /// _country return country where service located
--- test/command/command_url_test.dart
+++ test/command/command_url_test.dart
@@ -5,31 +5,38 @@ import 'package:libcli/i18n.dart' as i18n;
 void main() {
   mockCommand();
   group('[command_url_test]', () {
-    test('should use production url', () async {
+    test('should use stable url', () async {
+      serviceBranch = BRANCH_STABLE;
+      serviceRegion = 'us';
+      expect(serviceUrl('mock'), 'https://mock-us-stable.piyuo.com/?q');
+    });
+
+    test('should use master url', () async {
       serviceBranch = BRANCH_MASTER;
       serviceRegion = 'us';
-      expect(serviceUrl('mock'), 'https://mock-us.api.piyuo.com');
+      expect(serviceUrl('mock'), 'https://mock-us-master.piyuo.com/?q');
     });
 
     test('should use defult url', () async {
       i18n.userPreferCountryCode = null;
       serviceRegion = null;
       serviceBranch = BRANCH_MASTER;
-      expect(serviceUrl('mock'), 'https://mock-us.api.piyuo.com');
+      expect(serviceUrl('mock'), 'https://mock-us-master.piyuo.com/?q');
     });
 
     test('should use beta url', () async {
       i18n.userPreferCountryCode = 'tw';
       serviceRegion = null;
       serviceBranch = BRANCH_BETA;
-      expect(serviceUrl('mock'), 'https://mock-jp.beta.piyuo.com');
+      //tw using jp data center
+      expect(serviceUrl('mock'), 'https://mock-jp-beta.piyuo.com/?q');
     });
 
     test('should use service country', () async {
       i18n.userPreferCountryCode = 'tw';
       serviceRegion = 'us';
       serviceBranch = BRANCH_BETA;
-      expect(serviceUrl('mock'), 'https://mock-us.beta.piyuo.com');
+      expect(serviceUrl('mock'), 'https://mock-us-beta.piyuo.com/?q');
     });
   });
 }
