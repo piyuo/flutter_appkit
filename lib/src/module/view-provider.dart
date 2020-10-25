879e0fe65331248cab07dcc10de74d1db2364443
piyuo <piyuo.com@gmail.com>
2020-10-25 12:49:51 -0700

    using moduleProvider instead of reduxProvider

--- lib/src/module/view-provider.dart
+++ lib/src/module/view-provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:libcli/module.dart';
-import 'package:libcli/redux.dart';
 import 'package:libcli/i18n.dart';
 import 'package:libcli/eventbus.dart' as eventbus;
 
@@ -11,19 +10,19 @@ class ViewProvider extends AsyncProvider {
   ///     var state=provider.dispatch(context,Increment(1));
   ///
   Future<Map> dispatch(BuildContext context, dynamic action) async {
-    final redux = Provider.of<ReduxProvider>(context, listen: false);
-    assert(redux != null, 'redux provider not found');
-    await redux.dispatch(context, action);
-    return redux.state;
+    final module = Provider.of<ModuleProvider>(context, listen: false);
+    assert(module != null, 'module provider not found');
+    await module.dispatch(context, action);
+    return module.state;
   }
 
   /// errCheck check redux state['err'] return true if no error, brodcast [ShowErrorEvent] if receive error code
   ///
   bool errCheck(BuildContext context) {
-    final redux = Provider.of<ReduxProvider>(context, listen: false);
-    assert(redux != null, 'redux provider not found');
+    final module = Provider.of<ModuleProvider>(context, listen: false);
+    assert(module != null, 'module provider not found');
 
-    String err = redux.state['err'];
+    String err = module.state['err'];
     if (err == null) {
       return false;
     } else if (err != '') {
