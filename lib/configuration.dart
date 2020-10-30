2ac2b878433a8f60821b463d01f0da005af9ffbc
piyuo <piyuo.com@gmail.com>
2020-10-30 14:18:52 -0700

    export web/native

--- lib/configuration.dart
+++ lib/configuration.dart
@@ -1,3 +1,5 @@
 library configuration;
 
 export 'package:libcli/src/configuration/configuration.dart';
+export 'package:libcli/src/configuration/native.dart';
+export 'package:libcli/src/configuration/web.dart';
