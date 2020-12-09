d86978cd5b8481e8585d665eed8373c20aab01aa
piyuo <piyuo.com@gmail.com>
2020-12-08 18:14:47 -0800

    remove material dependency

--- lib/src/app/web/web.dart
+++ lib/src/app/web/web.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:libcli/src/app/configuration.dart' as config;
 import 'package:libcli/log.dart';
