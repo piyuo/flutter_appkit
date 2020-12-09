a9d3c905a20ad7dbe530a69f7e8cc45c62c78552
piyuo <piyuo.com@gmail.com>
2020-12-08 18:06:18 -0800

    remove material

--- lib/src/eventbus/contract_test.dart
+++ lib/src/eventbus/contract_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:libcli/src/eventbus/contract.dart';
 import 'package:libcli/src/eventbus/eventbus.dart';
 import 'package:mockito/mockito.dart';
--- lib/src/eventbus/eventbus.dart
+++ lib/src/eventbus/eventbus.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:libcli/log.dart';
 import 'package:libcli/src/eventbus/contract.dart';
 
