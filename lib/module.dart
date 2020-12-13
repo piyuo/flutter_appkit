b6d63975ece7d446967e280a455e2aa71db44d03
piyuo <piyuo.com@gmail.com>
2020-12-12 23:05:48 -0800

    remove progress indicator

--- lib/module.dart
+++ lib/module.dart
@@ -4,6 +4,5 @@ export 'package:libcli/src/module/async-provider.dart';
 export 'package:libcli/src/module/await.dart';
 export 'package:libcli/src/module/view-widget.dart';
 export 'package:libcli/src/module/view-provider.dart';
-export 'package:libcli/src/module/await-progress-indicator.dart';
 export 'package:libcli/src/module/await-error-message.dart';
 export 'package:libcli/src/module/module-provider.dart';
