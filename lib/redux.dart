71c63ff052a1fa8d4e7a0a43ff9c09deaf1af7f9
piyuo <piyuo.com@gmail.com>
2020-12-14 23:11:03 -0800

    export mock redux

--- lib/redux.dart
+++ lib/redux.dart
@@ -1,5 +1,6 @@
 library redux;
 
 export 'package:libcli/src/redux/redux.dart';
+export 'package:libcli/src/redux/mock-redux.dart';
 export 'package:libcli/src/redux/history-redux.dart';
 export 'package:libcli/src/redux/map.dart';
