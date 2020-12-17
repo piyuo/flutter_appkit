840cff9723157fa29222112d3895aff27fda1705
piyuo <piyuo.com@gmail.com>
2020-12-17 14:58:31 -0800

    pass test

--- lib/src/redux/redux_test.dart
+++ lib/src/redux/redux_test.dart
@@ -19,7 +19,7 @@ void main() {
 
     test('should convert states to string', () {
       var result = stateToStr({'a': 0, 'b': 1});
-      expect(result, '[{"a":0,"b":1}');
+      expect(result, '{"a":0,"b":1}');
     });
 
     test('should return false cause state not change', () async {
