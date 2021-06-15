419a2a645154355ff7526bdad40725b061d0e1c6
piyuo <piyuo.com@gmail.com>
2021-06-15 10:46:10 -0700

    add equals

--- lib/src/types/latlng.dart
+++ lib/src/types/latlng.dart
@@ -13,4 +13,9 @@ class LatLng {
   static LatLng empty = LatLng(0, 0);
 
   bool get isEmpty => lat == 0;
+
+  /// Equals return true if value is equal
+  bool Equals(LatLng value) {
+    return lat == value.lat && lng == value.lng;
+  }
 }
---
+++ lib/src/types/latlng_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'latlng.dart';
+
+void main() {
+  group('[LatLng]', () {
+    test('should return true when equal', () async {
+      final l = LatLng(49.4540877, -173.7548384);
+      final l2 = LatLng(49.4540877, -173.7548384);
+      expect(l.Equals(l2), true);
+    });
+    test('should return false when not equal', () async {
+      final l = LatLng(49.4540877, -173.7548384);
+      final l2 = LatLng(49, -173);
+      expect(l.Equals(l2), false);
+    });
+  });
+}
