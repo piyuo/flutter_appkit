b0e6f9fb4cac8f55e3a9203bcf3640e29b070d60
piyuo <piyuo.com@gmail.com>
2021-06-19 22:03:01 -0700

    update locate export

--- lib/locate.dart
+++ lib/locate.dart
@@ -1,4 +1,4 @@
 library locationService;
 
-export 'package:libcli/src/maps/location.dart';
-export 'package:libcli/src/maps/map.dart';
+export 'package:libcli/src/locate/location.dart';
+export 'package:libcli/src/locate/map.dart';
