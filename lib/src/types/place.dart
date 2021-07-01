00ddd0dd5969f15e9e040e3b0ea49a6364ca23bf
piyuo <piyuo.com@gmail.com>
2021-06-30 21:52:06 -0700

    add country to place

--- lib/src/types/place.dart
+++ lib/src/types/place.dart
@@ -1,23 +1,34 @@
 import 'package:libcli/types.dart' as types;
 
+/// Place is geographic location
 class Place {
   Place({
     required this.address,
     required this.latlng,
     required this.tags,
+    required this.country,
   });
 
+  /// address is official address, but not contain floor or room
   final String address;
 
+  // latlng is this place coordinate
   final types.LatLng latlng;
 
+  /// tags is a list of tag about this place like 'SPECTRUM', 'IRVINE', 'CA', 'US'
   final List<String> tags;
 
+  /// country code like 'US', 'CN', 'AU'
+  final String country;
+
+  /// empty instance of place
   static Place empty = Place(
-    address: "",
+    address: '',
     latlng: types.LatLng.empty,
     tags: [],
+    country: '',
   );
 
+  /// isEmpty return true if this place is empty
   bool get isEmpty => latlng.isEmpty;
 }
