7e1066bebd4fff22a23ffeb5a6c9adac3b1b88b6
piyuo <piyuo.com@gmail.com>
2021-07-11 19:41:00 -0700

    add sample code

--- lib/src/mocking/context.dart
+++ lib/src/mocking/context.dart
@@ -7,6 +7,9 @@ import 'package:flutter/widgets.dart' as widgets;
 class Context extends Mock implements widgets.BuildContext {}
 
 /// useTestFont set text scale to 1/2, cause font in flutter test is bigger than real device, need scale down
+///
+///     mocking.useTestFont(tester);
+///
 void useTestFont(WidgetTester tester) {
   tester.binding.window.textScaleFactorTestValue = 0.5; // test font is bigger than real device, need scale down
 }
