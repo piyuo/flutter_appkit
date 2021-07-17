d775ada70b5b92b2666ab5763d19a9a57640b59f
piyuo <piyuo.com@gmail.com>
2021-07-17 15:43:12 -0700

    simplify dict

--- lib/src/i18n/predefine.dart
+++ lib/src/i18n/predefine.dart
@@ -7,7 +7,7 @@ String predefine(String key) {
   var value = _dict[key];
   if (value == null) {
     log.log('${log.COLOR_ALERT}missing $key in i18n global');
-    return '!!! $key not found';
+    return '!! $key not found';
   }
   return value;
 }
@@ -31,10 +31,9 @@ Map get _dict {
 }
 
 Map get _enUS {
-  var tryAgain = ', Please try again later';
   return {
     'errTitle': 'Oops, something went wrong',
-    'notified': 'The developer team has been notified of this issue${tryAgain}',
+    'notified': 'The developer team has been notified of this issue, Please try again later',
     'error': 'Error',
     'emailUs': 'Send email to support team',
     'email': 'Email',
@@ -58,7 +57,7 @@ Map get _enUS {
     'minLength': '%1 must contain at least %2 character. You entered %3 characters',
     'maxLength': '%1 must be %2 characters or fewer. You entered %3 characters',
     'noService': 'Service Unavailable',
-    'noServiceDesc': 'This service isn\'t available right now${tryAgain}',
+    'noServiceDesc': 'This service isn\'t available right now, Please try again later',
     'noInternet': 'No internet!',
     'noInternetDesc': 'Poor network connection detected, Please check your connectivity',
     'blocked': 'Internet blocked!',
@@ -66,19 +65,18 @@ Map get _enUS {
     'diskError': 'Write failed!',
     'diskErrorDesc': 'Insufficient disk space or write access denied',
     'timeout': 'Operation timed out!',
-    'timeoutDesc': 'Your operation didn\'t complete in time${tryAgain}',
+    'timeoutDesc': 'Your operation didn\'t complete in time, Please try again later',
     'BLOCK_SHORT': 'Processing Request. Please Wait',
     'BLOCK_LONG': 'Too many failed attempts, Please try again tomorrow',
     'IN_FLIGHT': 'Please wait while we are processing your request',
-    'OVERFLOW': 'Too many attempts${tryAgain}',
+    'OVERFLOW': 'Too many attempts, Please try again later',
   };
 }
 
 Map get _zhTW {
-  var tryAgain = '，請稍後再試';
   return {
     'errTitle': '糟糕，有東西出錯了',
-    'notified': '發生的錯誤已被記錄並且通知了我們的開發團隊${tryAgain}',
+    'notified': '發生的錯誤已被記錄並且通知了我們的開發團隊，請稍後再試',
     'error': '錯誤',
     'emailUs': '發送電子郵件給我們',
     'errCode': '錯誤代碼: ',
@@ -103,7 +101,7 @@ Map get _zhTW {
     'minLength': '%1必須至少包含 %2 個字元. 你輸入了 %3 個字元',
     'maxLength': '%1必須是%2個或更少字元. 你輸入了 %3 個字元',
     'noService': '服務不可用',
-    'noServiceDesc': '此服務當前不可用${tryAgain}',
+    'noServiceDesc': '此服務當前不可用，請稍後再試',
     'noInternet': '無法連接到網際網路!',
     'noInternetDesc': '請檢查你的網路連線或稍後重試',
     'blocked': '我們的網站可能被封鎖了!',
@@ -111,19 +109,18 @@ Map get _zhTW {
     'diskError': '寫入錯誤!',
     'diskErrorDesc': '請檢查是否空間不足或是權限不夠',
     'timeout': '操作已經超時!',
-    'timeoutDesc': '您的操作沒有得到伺服器回應，可能是伺服器目前忙碌中${tryAgain}',
+    'timeoutDesc': '您的操作沒有得到伺服器回應，可能是伺服器目前忙碌中，請稍後再試',
     'BLOCK_SHORT': '處理中，請稍候',
     'BLOCK_LONG': '您嚐試了太多失敗的請求, 請明天再試',
     'IN_FLIGHT': '請求正在處理中，請稍候',
-    'OVERFLOW': '操作過於頻繁${tryAgain}',
+    'OVERFLOW': '操作過於頻繁，請稍後再試',
   };
 }
 
 Map get _zhCN {
-  var tryAgain = '，请稍后再试';
   return {
     'errTitle': '糟了，有东西出错',
-    'notified': '发生的错误已被记录并且通知了我们的开发团队${tryAgain}',
+    'notified': '发生的错误已被记录并且通知了我们的开发团队，请稍后再试',
     'error': '错误',
     'email': '电子邮件',
     'emailUs': '發送电子邮件给我们',
@@ -148,7 +145,7 @@ Map get _zhCN {
     'minLength': '%1必须至少包含 %2 个字元. 您输入了 %3 个字元',
     'maxLength': '%1必须是%2个或更少字元. 您输入了 %3 个字元',
     'noService': '服務不可用',
-    'noServiceDesc': '服務暫時不可用${tryAgain}',
+    'noServiceDesc': '服務暫時不可用，请稍后再试',
     'noInternet': '无法连接到互联网!',
     'noInternetDesc': '请检查你的网路连线或稍后重试',
     'blocked': '我们的网站可能被封锁了!',
@@ -156,10 +153,10 @@ Map get _zhCN {
     'diskError': '写入错误!',
     'diskErrorDesc': '请检查是否空间不足或是权限不够',
     'timeout': '操作已经超时!',
-    'timeoutDesc': '您的操作没有得到服务器回应，可能是服务器目前忙碌中${tryAgain}',
+    'timeoutDesc': '您的操作没有得到服务器回应，可能是服务器目前忙碌中，请稍后再试',
     'BLOCK_SHORT': '处理中，请稍候',
     'BLOCK_LONG': '您嚐试了太多失败的请求, 请明天再试',
     'IN_FLIGHT': '请求正在处理中，请稍候',
-    'OVERFLOW': '操作过于频繁${tryAgain}',
+    'OVERFLOW': '操作过于频繁，请稍后再试',
   };
 }
