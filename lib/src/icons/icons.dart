a2a7b30d87becbc323a19896d4e60a197085131f
piyuo <piyuo.com@gmail.com>
2021-07-21 12:03:36 -0700

    add more icon

--- lib/src/icons/icons.dart
+++ lib/src/icons/icons.dart
@@ -19,16 +19,17 @@ class CustomIcons {
   
   static const IconData alarmOn = _CustomIconsData(0xe000);
   static const IconData arrowBackIosNew = _CustomIconsData(0xe001);
-  static const IconData close = _CustomIconsData(0xe002);
-  static const IconData cloudOff = _CustomIconsData(0xe003);
-  static const IconData email = _CustomIconsData(0xe004);
-  static const IconData errorOutline = _CustomIconsData(0xe005);
-  static const IconData expandMore = _CustomIconsData(0xe006);
-  static const IconData menu = _CustomIconsData(0xe007);
-  static const IconData myLocation = _CustomIconsData(0xe008);
-  static const IconData navigateNext = _CustomIconsData(0xe009);
-  static const IconData priorityHigh = _CustomIconsData(0xe00a);
-  static const IconData search = _CustomIconsData(0xe00b);
-  static const IconData wifiOff = _CustomIconsData(0xe00c);
-  static const IconData wifi = _CustomIconsData(0xe00d);
+  static const IconData checkCircle = _CustomIconsData(0xe002);
+  static const IconData close = _CustomIconsData(0xe003);
+  static const IconData cloudOff = _CustomIconsData(0xe004);
+  static const IconData email = _CustomIconsData(0xe005);
+  static const IconData errorOutline = _CustomIconsData(0xe006);
+  static const IconData expandMore = _CustomIconsData(0xe007);
+  static const IconData menu = _CustomIconsData(0xe008);
+  static const IconData myLocation = _CustomIconsData(0xe009);
+  static const IconData navigateNext = _CustomIconsData(0xe00a);
+  static const IconData priorityHigh = _CustomIconsData(0xe00b);
+  static const IconData search = _CustomIconsData(0xe00c);
+  static const IconData wifiOff = _CustomIconsData(0xe00d);
+  static const IconData wifi = _CustomIconsData(0xe00e);
 }
