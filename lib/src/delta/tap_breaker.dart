0477a172331f609ea2b9f10f87b8a692f75ae2e5
piyuo <piyuo.com@gmail.com>
2021-10-03 21:16:22 -0700

    support value changed link

--- lib/src/delta/tap_breaker.dart
+++ lib/src/delta/tap_breaker.dart
@@ -3,21 +3,42 @@ import 'package:flutter/foundation.dart';
 
 /// TapBreak will break all concatenate callback if one callback is running
 class TapBreaker with ChangeNotifier {
-  bool _running = false;
+  bool _busy = false;
+
+  void setBusy(bool value) {
+    if (_busy != value) {
+      _busy = value;
+      notifyListeners();
+    }
+  }
 
   /// link add break on callback, if one callback is running the others will be disable
-  GestureTapCallback? link(Future<dynamic> Function()? callback) {
-    if (_running || callback == null) {
+  GestureTapCallback? link(Future Function()? callback) {
+    if (_busy || callback == null) {
       return null;
     }
     return () async {
-      _running = true;
+      setBusy(true);
       notifyListeners();
       try {
         await callback();
       } finally {
-        _running = false;
-        notifyListeners();
+        setBusy(false);
+      }
+    };
+  }
+
+  /// linkValueChanged add break on callback, if one callback is running the others will be disable
+  ValueChanged<T?>? linkValueChanged<T>(Future Function(T? value)? callback) {
+    if (_busy || callback == null) {
+      return null;
+    }
+    return (T? value) async {
+      setBusy(true);
+      try {
+        await callback(value);
+      } finally {
+        setBusy(false);
       }
     };
   }
