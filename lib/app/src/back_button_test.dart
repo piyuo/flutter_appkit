92df018be01c7db663bcff64c93f9104e1b8ee0e
piyuo <piyuo.com@gmail.com>
2021-10-22 12:56:42 -0700

    refactor test output

--- lib/app/src/back_button_test.dart
+++ lib/app/src/back_button_test.dart
@@ -3,7 +3,7 @@ import 'package:libcli/testing.dart' as testing;
 import 'back_button.dart';
 
 void main() {
-  group('[page_route]', () {
+  group('[app.back_button]', () {
     test('should return back button', () {
       expect(backButton(testing.Context()), isNull);
     });
