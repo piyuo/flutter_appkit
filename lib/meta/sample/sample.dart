cbdea04f9c8bed22caf2698ff2ca2a37837ec716
piyuo <piyuo.com@gmail.com>
2021-12-28 18:51:43 -0800

    add sample to test entity

---
+++ lib/meta/sample/sample.dart
@@ -0,0 +1,4 @@
+library sample;
+
+export 'src/sample-service.pb.dart';
+export 'src/person.pb.dart';
---
+++ lib/meta/sample/src/person.pb.dart
@@ -0,0 +1,93 @@
+///
+//  Generated code. Do not modify.
+//  source: person.proto
+//
+// @dart = 2.12
+// ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
+
+import 'dart:core' as $core;
+import 'package:libcli/pb/pb.dart' as pb;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+import 'package:libcli/pb/src/common/src/entity.pb.dart' as $0;
+
+class Person extends pb.Object {
+  $core.int mapIdXXX() => 1001;
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(const $core.bool.fromEnvironment('protobuf.omit_message_names') ? '' : 'Person', createEmptyInstance: create)
+    ..aOM<$0.Entity>(1, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'entity', subBuilder: $0.Entity.create)
+    ..aOS(2, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'name')
+    ..a<$core.int>(3, const $core.bool.fromEnvironment('protobuf.omit_field_names') ? '' : 'age', $pb.PbFieldType.O3)
+    ..hasRequiredFields = false
+  ;
+
+  Person._() : super();
+  factory Person({
+    $0.Entity? entity,
+    $core.String? name,
+    $core.int? age,
+  }) {
+    final _result = create();
+    if (entity != null) {
+      _result.entity = entity;
+    }
+    if (name != null) {
+      _result.name = name;
+    }
+    if (age != null) {
+      _result.age = age;
+    }
+    return _result;
+  }
+  factory Person.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory Person.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  Person clone() => Person()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  Person copyWith(void Function(Person) updates) => super.copyWith((message) => updates(message as Person)) as Person; // ignore: deprecated_member_use
+  $pb.BuilderInfo get info_ => _i;
+  @$core.pragma('dart2js:noInline')
+  static Person create() => Person._();
+  Person createEmptyInstance() => create();
+  static $pb.PbList<Person> createRepeated() => $pb.PbList<Person>();
+  @$core.pragma('dart2js:noInline')
+  static Person getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<Person>(create);
+  static Person? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $0.Entity get entity => $_getN(0);
+  @$pb.TagNumber(1)
+  set entity($0.Entity v) { setField(1, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasEntity() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearEntity() => clearField(1);
+  @$pb.TagNumber(1)
+  $0.Entity ensureEntity() => $_ensure(0);
+
+  @$pb.TagNumber(2)
+  $core.String get name => $_getSZ(1);
+  @$pb.TagNumber(2)
+  set name($core.String v) { $_setString(1, v); }
+  @$pb.TagNumber(2)
+  $core.bool hasName() => $_has(1);
+  @$pb.TagNumber(2)
+  void clearName() => clearField(2);
+
+  @$pb.TagNumber(3)
+  $core.int get age => $_getIZ(2);
+  @$pb.TagNumber(3)
+  set age($core.int v) { $_setSignedInt32(2, v); }
+  @$pb.TagNumber(3)
+  $core.bool hasAge() => $_has(2);
+  @$pb.TagNumber(3)
+  void clearAge() => clearField(3);
+}
+
