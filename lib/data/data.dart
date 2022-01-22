c810f849dcbd9e1fc69eaa0ec955c1f577deb482
piyuo <piyuo.com@gmail.com>
2022-01-22 11:47:27 -0800

    export datapod

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -3,6 +3,7 @@ library data;
 export 'src/languages.dart';
 export 'src/data.dart';
 export 'src/dataset.dart';
+export 'src/data_pod.dart';
 export 'src/data_source.dart';
 export 'src/page_table.dart';
 export 'src/page_list.dart';
