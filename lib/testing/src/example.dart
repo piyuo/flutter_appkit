0bfef3cf8a50ae5f1aed02cf754752290d4b0cfb
piyuo <piyuo.com@gmail.com>
2022-02-14 11:24:31 -0800

    refactor example button using builder pattern

--- lib/testing/src/example.dart
+++ lib/testing/src/example.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
 
-Widget _button(BuildContext context, String text, void Function() callback) {
-  return Padding(
-    padding: const EdgeInsets.all(10),
-    child: ElevatedButton(child: Text(text), onPressed: callback),
-  );
-}
+/// ExampleButton is button that will navigate to example builder
+/// ```dart
+/// ExampleButton(
+///  label: 'example1',
+///  builder: ()=>Container(),
+///  )
+/// ```
+class ExampleButton extends StatelessWidget {
+  /// ExampleButton is button that will navigate to example builder
+  /// ```dart
+  /// ExampleButton(
+  ///  label: 'example1',
+  ///  builder: ()=>Container(),
+  ///  )
+  /// ```
+  const ExampleButton({
+    Key? key,
+    required this.label,
+    required this.builder,
+    this.useScaffold = true,
+  }) : super(key: key);
 
-void _show(
-  BuildContext context,
-  Widget child, {
-  bool useScaffold = true,
-}) {
-  Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => useScaffold
-            ? Scaffold(
-                appBar: AppBar(),
-                body: SafeArea(child: child),
-              )
-            : child,
-      ));
-}
+  /// label is button label
+  final String label;
 
-Widget example(
-  BuildContext context, {
-  String? text,
-  Widget? child,
-  bool useScaffold = true,
-}) {
-  assert(text != null);
-  assert(child != null);
-  return _button(context, text!, () => _show(context, child!, useScaffold: useScaffold));
+  /// builder is function that will be called when button is pressed
+  final Widget Function() builder;
+
+  /// useScaffold is true if you want to use scaffold
+  final bool useScaffold;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ElevatedButton(
+          child: Text(label),
+          onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => useScaffold
+                    ? Scaffold(
+                        appBar: AppBar(),
+                        body: SafeArea(child: builder()),
+                      )
+                    : builder(),
+              ))),
+    );
+  }
 }
