9eb098c74e9e7bdfcbd1f29ad994b814ac7c6773
piyuo <piyuo.com@gmail.com>
2022-05-05 16:10:04 -0700

    add show animation option to loading display

--- lib/delta/src/indicator.dart
+++ lib/delta/src/indicator.dart
@@ -4,6 +4,66 @@ import 'package:flutter/material.dart';
 import 'package:loading_indicator/loading_indicator.dart';
 import 'package:libcli/i18n/i18n.dart' as i18n;
 
+/// LoadingDisplay show a loading display on page
+class LoadingDisplay extends StatelessWidget {
+  const LoadingDisplay({
+    Key? key,
+  }) : super(key: key);
+
+  final bool 
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        height: 250,
+        width: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+                width: 100,
+                height: 100,
+                child: LoadingIndicator(
+                  indicatorType: Indicator.lineScalePulseOut,
+                  colors: kDefaultRainbowColors,
+                  strokeWidth: 4.0,
+                )),
+            const SizedBox(height: 25),
+            Text(context.i18n.loadingLabel,
+                style: const TextStyle(
+                  color: Colors.grey,
+                )),
+          ],
+        ),
+      );
+}
+
+/// NoDataDisplay show a no data display on page
+class NoDataDisplay extends StatelessWidget {
+  const NoDataDisplay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        height: 250,
+        width: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.wb_cloudy_outlined,
+              size: 54,
+              color: Colors.grey,
+            ),
+            Text(context.i18n.noDataLabel,
+                style: const TextStyle(
+                  color: Colors.grey,
+                )),
+          ],
+        ),
+      );
+}
+
 const List<Color> kDefaultRainbowColors = [
   Colors.red,
   Colors.orange,
@@ -67,61 +127,3 @@ Widget ballRotateChase({
       colors: colors,
       strokeWidth: 12.0,
     );
-
-/// NoDataDisplay show a no data display on page
-class NoDataDisplay extends StatelessWidget {
-  const NoDataDisplay({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => SizedBox(
-        height: 250,
-        width: 250,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.wb_cloudy_outlined,
-              size: 54,
-              color: Colors.grey,
-            ),
-            Text(context.i18n.noDataLabel,
-                style: const TextStyle(
-                  color: Colors.grey,
-                )),
-          ],
-        ),
-      );
-}
-
-/// LoadingDisplay show a loading display on page
-class LoadingDisplay extends StatelessWidget {
-  const LoadingDisplay({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => SizedBox(
-        height: 250,
-        width: 250,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-                width: 100,
-                height: 100,
-                child: LoadingIndicator(
-                  indicatorType: Indicator.lineScalePulseOut,
-                  colors: kDefaultRainbowColors,
-                  strokeWidth: 4.0,
-                )),
-            const SizedBox(height: 25),
-            Text(context.i18n.loadingLabel,
-                style: const TextStyle(
-                  color: Colors.grey,
-                )),
-          ],
-        ),
-      );
-}
