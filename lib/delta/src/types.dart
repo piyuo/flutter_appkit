2b41c77c1a1861679d5223fc1d50c28eab2ebd4a
piyuo <piyuo.com@gmail.com>
2022-05-21 18:50:10 -0700

    Add WidgetContextBuilder

--- lib/delta/src/types.dart
+++ lib/delta/src/types.dart
@@ -7,4 +7,7 @@ typedef FutureCallback<T> = Future<T> Function();
 typedef FutureContextCallback<T> = Future<T> Function(BuildContext context);
 
 /// WidgetBuilder build widget
-typedef WidgetBuilder<T> = Widget Function(BuildContext context);
+typedef WidgetBuilder<T> = Widget Function();
+
+/// WidgetContextBuilder build widget with context
+typedef WidgetContextBuilder<T> = Widget Function(BuildContext context);
