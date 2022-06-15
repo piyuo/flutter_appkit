361efaedd8a72c1cec3122d2eb55a3bbdc38d83f
piyuo <piyuo.com@gmail.com>
2022-06-14 20:40:08 -0700

    Pass test

--- lib/database/src/database_test.dart
+++ lib/database/src/database_test.dart
@@ -18,36 +18,40 @@ void main() {
 
   group('[database]', () {
     test('should delete', () async {
-      final database = await open('test_db');
+      final database = await open('test_delete');
       await database.setBool('k', true);
       expect(database.contains('k'), true);
       await database.delete('k');
       expect(database.contains('k'), false);
+      await delete('test_delete');
     });
 
     test('should set/get bool', () async {
-      final database = await open('test_db');
+      final database = await open('test_bool');
       await database.setBool('k', false);
       final value = await database.getBool('k');
       expect(value, false);
+      await delete('test_bool');
     });
 
     test('should set/get int', () async {
-      final database = await open('test_db');
+      final database = await open('test_int');
       await database.setInt('k', 1);
       final value = await database.getInt('k');
       expect(value, 1);
+      await delete('test_int');
     });
 
     test('should set/get string', () async {
-      final database = await open('test_db');
+      final database = await open('test_string');
       await database.setString('k', 'hi');
       final value = await database.getString('k');
       expect(value, 'hi');
+      await delete('test_string');
     });
 
     test('should set/get datetime', () async {
-      final database = await open('test_db');
+      final database = await open('test_datetime');
       final now = DateTime.now();
       await database.setDateTime('k', now);
       final value = await database.getDateTime('k');
@@ -56,43 +60,48 @@ void main() {
       expect(now.day, value.day);
       expect(now.hour, value.hour);
       expect(now.minute, value.minute);
+      await delete('test_datetime');
     });
 
     test('should set/get pb.object', () async {
-      final database = await open('test_db');
+      final database = await open('test_object');
       final person = sample.Person(name: 'l');
       await database.setObject('k', person);
       final value = await database.getObject<sample.Person>('k', () => sample.Person());
       expect(value, isNotNull);
       expect(value!.name, 'l');
       expect(value, person);
+      await delete('test_object');
     });
 
     test('should save string list', () async {
-      final database = await open('test_db');
+      final database = await open('test_string_list');
       final list = <String>['1', '2', '3'];
       await database.setStringList('l', list);
       var list2 = await database.getStringList('l');
       expect(list2!.length, 3);
       expect(list2[2], '3');
+      await delete('test_string_list');
     });
 
     test('should save int list', () async {
-      final database = await open('test_db');
+      final database = await open('test_list');
       final list = <int>[1, 2, 3];
       await database.setIntList('l', list);
       var list2 = await database.getIntList('l');
       expect(list2!.length, 3);
       expect(list2[2], 3);
+      await delete('test_list');
     });
 
     test('should reset', () async {
-      final database = await open('test_db');
+      final database = await open('test_reset');
       await database.setString('a', 'b');
       await database.setString('1', '2');
       await database.reset();
       expect(database.contains('a'), false);
       expect(database.contains('1'), false);
+      await delete('test_reset');
     });
   });
 }
