394fd5b6970441ece302bb0c73a2a7969a641127
piyuo <piyuo.com@gmail.com>
2022-06-30 12:42:26 -0700

    Stop support amap fow now
    They stop update flutter plugin

--- lib/locate/src/platform.dart
+++ lib/locate/src/platform.dart
@@ -1,13 +1,12 @@
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:libcli/i18n/i18n.dart' as i18n;
+//import 'package:libcli/i18n/i18n.dart' as i18n;
 import 'package:libcli/types/types.dart' as types;
 import 'map_google.dart';
 import 'map_apple.dart';
-import 'map_amap.dart';
 import 'map.dart';
 
-enum MapType { google, apple, amap }
+enum MapType { google, apple } //, amap
 
 MapType mapType() {
   if (kIsWeb) {
@@ -21,9 +20,9 @@ MapType mapType() {
   }
 
   // use amap in china, cause google map may not work in china
-  if (i18n.countryCode == 'CN') {
-    return MapType.amap;
-  }
+  //if (i18n.countryCode == 'CN') {
+  //  return MapType.amap;
+  //}
 
   //debug
   // return MapType.amap;
@@ -40,8 +39,8 @@ Map map() {
   switch (mapType()) {
     case MapType.apple:
       return const MapApple();
-    case MapType.amap:
-      return const MapAMap();
+    //case MapType.amap:
+    //  return const MapAMap();
     default:
       return const MapGoogle();
   }
@@ -55,8 +54,8 @@ MapProvider mapProvider() {
   switch (mapType()) {
     case MapType.apple:
       return MapProvider(AppleImpl());
-    case MapType.amap:
-      return MapProvider(AmapImpl());
+    //case MapType.amap:
+    //  return MapProvider(AmapImpl());
     default:
       return MapProvider(GoogleImpl());
   }
@@ -70,16 +69,16 @@ String mapUrl(String address, types.LatLng latlng) {
   String adr = Uri.encodeComponent(address);
   var t = mapType();
   if (t == MapType.apple) {
-    if (i18n.countryCode == 'CN') {
-      t = MapType.amap;
-    } else {
-      t = MapType.google;
-    }
+    //if (i18n.countryCode == 'CN') {
+    //  t = MapType.amap;
+    //  } else {
+//    }
+    t = MapType.google;
   }
 
   switch (t) {
-    case MapType.amap: // amap is lng first
-      return 'https://uri.amap.com/marker?position=${latlng.lng},${latlng.lat}&name=$adr&callnative=1';
+    //case MapType.amap: // amap is lng first
+    // return 'https://uri.amap.com/marker?position=${latlng.lng},${latlng.lat}&name=$adr&callnative=1';
     //case MapType.apple: // apple map is not accurate in china
     //  return 'http://maps.apple.com/?address=$adr&z=18';
     default:
