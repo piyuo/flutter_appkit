0f793d0e26d6c55b2434153a472a7a7f1f8c5976
piyuo <piyuo.com@gmail.com>
2022-08-11 22:00:29 -0700

    switch location to geolocator

--- lib/locate/src/location.dart
+++ lib/locate/src/location.dart
@@ -1,11 +1,41 @@
-import 'package:location/location.dart';
-//import 'package:geolocator/geolocator.dart';
+//import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:libcli/types/types.dart' as types;
 import 'package:libcli/log/log.dart' as log;
 
 /// deviceLatLng return device location info, return empty if can't not get device location (user not allow)
 /// this function is slow, it may takes few seconds to complete
 ///
+
+Future<types.LatLng> deviceLatLng() async {
+  bool serviceEnabled;
+  LocationPermission permissionGranted;
+  Position locationData;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return types.LatLng.empty;
+  }
+
+  permissionGranted = await Geolocator.checkPermission();
+  if (permissionGranted == LocationPermission.denied) {
+    permissionGranted = await Geolocator.requestPermission();
+    if (permissionGranted == LocationPermission.denied) {
+      return types.LatLng.empty;
+    }
+  }
+
+  try {
+    locationData = await Geolocator.getCurrentPosition();
+    return types.LatLng(locationData.latitude, locationData.longitude);
+  } catch (e, s) {
+    log.error(e, s);
+  }
+  return types.LatLng.empty;
+}
+
+
+/*
 Future<types.LatLng> deviceLatLng() async {
   Location location = Location();
 
@@ -38,32 +68,4 @@ Future<types.LatLng> deviceLatLng() async {
   return types.LatLng.empty;
 }
 
-
-/*
-Future<types.LatLng> deviceLatLngGeolocator() async {
-  bool _serviceEnabled;
-  LocationPermission _permissionGranted;
-  Position _locationData;
-
-  _serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!_serviceEnabled) {
-    return types.LatLng.empty;
-  }
-
-  _permissionGranted = await Geolocator.checkPermission();
-  if (_permissionGranted == LocationPermission.denied) {
-    _permissionGranted = await Geolocator.requestPermission();
-    if (_permissionGranted == LocationPermission.denied) {
-      return types.LatLng.empty;
-    }
-  }
-
-  try {
-    _locationData = await Geolocator.getCurrentPosition();
-    return types.LatLng(_locationData.latitude, _locationData.longitude);
-  } catch (e, s) {
-    log.error(e, s);
-  }
-  return types.LatLng.empty;
-}
 */
