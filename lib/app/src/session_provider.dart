aa416783124ef4d184005d7cddf08e62edab7970
piyuo <piyuo.com@gmail.com>
2023-01-29 14:40:08 -0800

    Define common session variable

--- lib/app/src/session_provider.dart
+++ lib/app/src/session_provider.dart
@@ -30,6 +30,15 @@ const _kTicketToken = 'T';
 /// _kTicketExpired is expired key
 const _kTicketExpired = 'E';
 
+/// kSessionUserName is the key for the user name in the session.
+const kSessionUserName = 'uName';
+
+/// kSessionUserPhoto is the key for the user photo in the session.
+const kSessionUserPhoto = 'uPhoto';
+
+/// kSessionRegion is the key for the region in the session.
+const kSessionRegion = 'region';
+
 /// Ticket keep token and expired time
 class Ticket {
   Ticket({
