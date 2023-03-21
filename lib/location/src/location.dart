09716b76fde80a2ed0aa21511896f3691c48eea3
piyuo <piyuo.com@gmail.com>
2023-03-20 19:06:12 -0700

    Handle location denied exception

--- lib/location/src/location.dart
+++ lib/location/src/location.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:libcli/general/general.dart' as general;
 import 'package:libcli/permission/permission.dart' as permission;
+import 'package:libcli/log/log.dart' as log;
 
 /// getCurrentLocation return device current location info, return empty if can't not get device location (user not allow)
 /// this function is slow, it may takes few seconds to complete
@@ -10,11 +11,16 @@ import 'package:libcli/permission/permission.dart' as permission;
 /// ```
 Future<general.LatLng?> getCurrentLocation(String reason) async {
   if (await permission.getLocationPermission(reason)) {
-    Position locationData = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-    debugPrint('lat: ${locationData.latitude}, lng: ${locationData.longitude}');
-    return general.LatLng(locationData.latitude, locationData.longitude);
+    try {
+      Position locationData = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      debugPrint('lat: ${locationData.latitude}, lng: ${locationData.longitude}');
+      return general.LatLng(locationData.latitude, locationData.longitude);
+    } catch (e, s) {
+      log.error(e, s);
+    }
+    return general.LatLng.empty;
   }
   debugPrint('user not allow location permission');
   return null;
