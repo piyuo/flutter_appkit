dff282f3a297868211db5223d5376a5799937bf5
piyuo <piyuo.com@gmail.com>
2023-07-16 21:45:15 -0700

    Support isImage/isVideo/isAudio in xFile

--- lib/pb/src/util.dart
+++ lib/pb/src/util.dart
@@ -30,6 +30,15 @@ bool isMimeAudio(String mime) => mime.startsWith('audio/');
 
 /// FileUtil provide toFile method for XFile
 extension FileUtil on XFile {
+  /// isImage return true if mime is image
+  bool get isImage => isMimeImage(mimeTypeByLookup);
+
+  /// isVideo return true if mime is video
+  bool get isVideo => isMimeVideo(mimeTypeByLookup);
+
+  /// isAudio return true if mime is audio
+  bool get isAudio => isMimeAudio(mimeTypeByLookup);
+
   /// mimeTypeByLookup try to get mime type by lookupMimeType
   String get mimeTypeByLookup => mimeType ?? lookupMimeType(path) ?? 'application/octet-stream';
 
