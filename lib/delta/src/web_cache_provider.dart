ea1ae124811a58636de901b6cc9dfc6be4903b46
piyuo <piyuo.com@gmail.com>
2023-08-16 17:59:27 -0700

    Use single cache manager

--- lib/delta/src/web_cache_provider.dart
+++ lib/delta/src/web_cache_provider.dart
@@ -7,23 +7,17 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 bool get isCacheAllowed =>
     !kIsWeb && (UniversalPlatform.isAndroid || UniversalPlatform.isIOS || UniversalPlatform.isMacOS);
 
+/// _cacheManager is the cache manager
+BaseCacheManager _cacheManager = CacheManager(
+  Config(
+    'webCache',
+    stalePeriod: const Duration(days: 360),
+    maxNrOfCacheObjects: 1000,
+  ),
+);
+
 /// WebCacheProvider provide cache manager for android/ios/mac platform
 class WebCacheProvider with ChangeNotifier {
-  WebCacheProvider() {
-    if (isCacheAllowed) {
-      _cacheManager = CacheManager(
-        Config(
-          'webCache',
-          stalePeriod: const Duration(days: 360),
-          maxNrOfCacheObjects: 1000,
-        ),
-      );
-    }
-  }
-
-  /// _cacheManager is the cache manager
-  BaseCacheManager? _cacheManager;
-
   /// hasError return true if video has error
   bool hasError = false;
 
@@ -31,21 +25,12 @@ class WebCacheProvider with ChangeNotifier {
   Future<File?> getFileFromCache(String url) async {
     if (isCacheAllowed) {
       try {
-        FileInfo? fileInfo = await _cacheManager!.getFileFromCache(url);
-        fileInfo ??= await _cacheManager!.downloadFile(url);
+        FileInfo? fileInfo = await _cacheManager.getFileFromCache(url);
+        fileInfo ??= await _cacheManager.downloadFile(url);
         return fileInfo.file;
       } catch (_) {}
     }
     hasError = true;
     return null;
-  }
-
-  @override
-  void dispose() {
-    if (isCacheAllowed && _cacheManager != null) {
-      _cacheManager!.dispose();
-      _cacheManager = null;
-    }
-    super.dispose();
   }
 }
