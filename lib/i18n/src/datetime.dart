b4d70c3c60b8a044179dc11bfed1c49c6d6f9ac7
piyuo <piyuo.com@gmail.com>
2023-09-10 21:53:49 -0700

    Pass test

--- lib/i18n/src/datetime.dart
+++ lib/i18n/src/datetime.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
 import 'i18n.dart';
 
 /// minDateTime return the minimum date
