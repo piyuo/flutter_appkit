65f01aa342e8c18825e9a4f9dcca3d38c1b1533e
piyuo <piyuo.com@gmail.com>
2023-09-27 19:16:53 -0700

    Move provider to global package so other packages can access

---
+++ lib/global/language_provider.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:libcli/preferences/preferences.dart' as storage;
+import 'package:libcli/i18n/i18n.dart' as i18n;
+
+/// _kPreferredLocaleKey is preferred locale key in storage
+const _kPreferredLocaleKey = 'locale';
+
+/// LanguageProvider provide a way to change locale
+class LanguageProvider with ChangeNotifier {
+  LanguageProvider({
+    this.domain = '',
+  });
+
+  /// domain used to separate different app, in some app may user use different domain but same app, so we need separate them
+  String domain;
+
+  /// _storeKey is key in storage
+  String get _storeKey => '$domain$_kPreferredLocaleKey';
+
+  /// _limitSupportedLocales is subset of supported locales, used when backend only support part of app supported locales
+  List<Locale>? _limitSupportedLocales;
+
+  /// availableLocales return available locales
+  List<Locale> get _availableLocales => _limitSupportedLocales ?? i18n.supportedLocales.toList();
+
+  /// initWithPreferredLocale init with preferred locale
+  Future<void> init() async {
+    if (i18n.preferLocale != null) {
+      // session provider may set prefer locale for user, so we don't need set from storage
+      return;
+    }
+    final savedLocale = await _readLocale();
+    if (savedLocale != null) {
+      if (!isLocaleAvailable(savedLocale)) {
+        await _writeLocale(null);
+        return;
+      }
+
+      if (i18n.preferLocale != savedLocale) {
+        await i18n.setPreferLocale(savedLocale);
+        notifyListeners();
+      }
+    }
+  }
+
+  /// changeLocale set preferred locale, it often used when user change locale in setting page
+  Future<void> changeLocale(Locale? newLocale) async {
+    if (await _writeLocale(newLocale)) {
+      await i18n.setPreferLocale(newLocale);
+      notifyListeners();
+    }
+  }
+
+  /// limitSupportedLocales limit supported locales, this limit will not be saved in storage,
+  /// need call this function every time when app start
+  Future<void> limitSupportedLocales(List<Locale> limitLocales) async {
+    _limitSupportedLocales = limitLocales;
+    if (!isLocaleAvailable(i18n.locale)) {
+      await i18n.setPreferLocale(availableLocales.first);
+      notifyListeners();
+      return;
+    }
+  }
+
+  /// availableLocales return available locales, often used in display locales to user
+  Iterable<Locale> get availableLocales {
+    for (Locale locale in _availableLocales) {
+      if (locale == i18n.locale) {
+        _availableLocales.remove(locale);
+        _availableLocales.insert(0, locale);
+        break;
+      }
+    }
+    return _availableLocales;
+  }
+
+  /// isLocaleAvailable check a locale is available or not
+  bool isLocaleAvailable(Locale? aLocale) {
+    for (Locale locale in _availableLocales) {
+      if (locale == aLocale) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /// _readLocale read saved locale from storage
+  Future<Locale?> _readLocale() async {
+    final localeTemp = await storage.getStringWithExp(_storeKey);
+    if (localeTemp != null) {
+      return i18n.stringToLocale(localeTemp);
+    }
+    return null;
+  }
+
+  /// _writeLocale write preferred locale to storage
+  /// if user change one page's locale, other page need reflect change
+  Future<bool> _writeLocale(Locale? newLocale) async {
+    if (newLocale == null) {
+      await storage.remove(_storeKey);
+      return true;
+    }
+
+    if (!isLocaleAvailable(newLocale)) {
+      return false;
+    }
+
+    await storage.setString(_storeKey, newLocale.toString());
+    return true;
+  }
+
+  /// of get I18nProvider from context
+  static LanguageProvider of(BuildContext context) {
+    return Provider.of<LanguageProvider>(context, listen: false);
+  }
+}
---
+++ lib/global/session_provider.dart
@@ -0,0 +1,287 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:libcli/preferences/preferences.dart' as preferences;
+import 'package:libcli/eventbus/eventbus.dart' as eventbus;
+import 'package:libcli/auth/auth.dart' as auth;
+import 'package:libcli/net/net.dart' as net;
+//import 'package:libcli/log/log.dart' as log;
+import 'package:libcli/i18n/i18n.dart' as i18n;
+
+/// LoginEvent is event when user login through UI
+class LoginEvent {}
+
+/// LogoutEvent is event when user logout
+class LogoutEvent {}
+
+/// SessionLoader called when session expired,
+/// you should use refresh key to exchange new access token, or display UI to let user log back in
+/// return null if you want to logout, or return new session
+typedef SessionLoader = Future<Session?> Function(Token? refreshToken);
+
+/// _kAccessTokenKey is access token key in preferences
+const _kAccessTokenKey = 'A';
+
+/// _kRefreshTokenKey is refresh token key in preferences
+const _kRefreshTokenKey = 'R';
+
+/// _kArgsKey is args key in preferences
+const _kArgsKey = 'G';
+
+/// _kTokenValueKey is token value key
+const _kTokenValueKey = 'V';
+
+/// _kTokenExpiredKey is token expired key
+const _kTokenExpiredKey = 'E';
+
+/// kSessionUserNameKey is the key for the user name in the session.
+const kSessionUserNameKey = 'N';
+
+/// kSessionUserPhotoKey is the key for the user photo in the session.
+const kSessionUserPhotoKey = 'P';
+
+/// kSessionRegionKey is the key for the region in the session.
+const kSessionRegionKey = 'O';
+
+/// kSessionLocaleKey is the key for the locale in the session.
+const kSessionLocaleKey = 'L';
+
+/// Token keep value and expired time
+class Token {
+  Token({
+    required this.value,
+    required this.expired,
+  });
+
+  /// value is access token value or refresh token value
+  String value;
+
+  /// expired is access key expired time
+  DateTime expired;
+
+  /// isValid check if token is valid
+  bool get isValid => value.isNotEmpty && expired.isAfter(DateTime.now());
+
+  /// save save token to storage
+  Future<void> save(String prefix) async {
+    await preferences.setMap(prefix, {
+      _kTokenValueKey: value,
+      _kTokenExpiredKey: expired.millisecondsSinceEpoch,
+    });
+  }
+
+  /// load token from storage
+  static Future<Token?> load(String prefix) async {
+    final data = await preferences.getMap(prefix);
+    if (data != null) {
+      return Token(
+        value: data[_kTokenValueKey],
+        expired: DateTime.fromMillisecondsSinceEpoch(data[_kTokenExpiredKey]),
+      );
+    }
+    return null;
+  }
+}
+
+/// Session keep access token, refresh token and extra data
+class Session {
+  Session({
+    required this.accessToken,
+    this.refreshToken,
+    this.args = const {},
+  });
+
+  /// fromAccess create session from access
+  static Session fromAccess(auth.Access access) {
+    return Session(
+      accessToken: Token(
+        value: access.accessToken,
+        expired: access.accessExpire.toDateTime(toLocal: true),
+      ),
+      refreshToken: Token(
+        value: access.refreshToken,
+        expired: access.refreshExpire.toDateTime(toLocal: true),
+      ),
+      args: access.args,
+    );
+  }
+
+  /// accessToken keep access token value and expired time
+  Token accessToken;
+
+  /// refreshToken keep refresh token value and expired time
+  Token? refreshToken;
+
+  /// isValid return true if access token is valid
+  bool get isValid => accessToken.isValid;
+
+  /// canRefresh return true if there is refresh token and it is valid
+  bool get canRefresh => refreshToken != null && refreshToken!.isValid;
+
+  /// args can keep extra data like region, language, etc
+  Map<String, dynamic> args;
+
+  /// operator [] get args
+  operator [](String i) => args[i]; // get
+
+  /// operator []= set args
+  operator []=(String i, dynamic value) => args[i] = value; // set
+
+  /// containsKey check if args contains key
+  bool containsKey(String key) => args.containsKey(key);
+
+  /// save session to preferences
+  Future<void> save() async {
+    await accessToken.save(_kAccessTokenKey);
+    if (refreshToken != null) {
+      await refreshToken!.save(_kRefreshTokenKey);
+    }
+    if (args.isNotEmpty) {
+      await preferences.setMap(_kArgsKey, args);
+    }
+  }
+
+  /// load session from preferences
+  static Future<Session?> load() async {
+    final accessToken = await Token.load(_kAccessTokenKey);
+    if (accessToken != null) {
+      return Session(
+        accessToken: accessToken,
+        refreshToken: await Token.load(_kRefreshTokenKey),
+        args: await preferences.getMap(_kArgsKey) ?? {},
+      );
+    }
+    return null;
+  }
+
+  /// remove session from preferences
+  static Future<void> remove() async {
+    await preferences.remove(_kAccessTokenKey);
+    await preferences.remove(_kRefreshTokenKey);
+    await preferences.remove(_kArgsKey);
+  }
+
+  @override
+  String toString() {
+    return 'Session{accessToken: ${accessToken.expired}, refreshToken: ${refreshToken?.expired}, args: $args}';
+  }
+}
+
+/// SessionProvider keep session and provide session to other widget
+class SessionProvider with ChangeNotifier {
+  SessionProvider({
+    required this.loader,
+  }) {
+    subscription = eventbus.listen((event) async {
+      if (event is net.AccessTokenRevokedEvent) {
+        if (session != null) {
+          session!.accessToken.value = '';
+        }
+      }
+      if (event is net.ForceLogOutEvent) {
+        await logout();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    subscription.cancel();
+    super.dispose();
+  }
+
+  /// init session and refresh session if expired
+  Future<void> init() async {
+    session = await Session.load();
+    if (session != null) {
+      await getValidSession();
+      if (session!.isValid) {
+        final locale = session![kSessionLocaleKey];
+        if (locale != null) {
+          await i18n.setPreferLocale(Locale(locale));
+        }
+      }
+      notifyListeners();
+    }
+  }
+
+  /// of get SessionProvider from context
+  static SessionProvider of(BuildContext context) {
+    return Provider.of<SessionProvider>(context, listen: false);
+  }
+
+  /// accessTokenBuilder is used by apollo client to get access token to command service
+  static Future<String?> accessTokenBuilder(BuildContext context) async {
+    final sessionProvider = SessionProvider.of(context);
+    final session = await sessionProvider.getValidSession();
+    if (session == null) {
+      return null;
+    }
+    return session.accessToken.value;
+  }
+
+  late eventbus.Subscription subscription;
+
+  /// loader called when session expired and need refresh
+  final SessionLoader loader;
+
+  /// _session is current session, it may not valid if expired, use getValidSession to get valid session
+  Session? session;
+
+  /// getValidSession return valid session if valid and it will refresh session if expired
+  Future<Session?> getValidSession() async {
+    if (session != null && session!.isValid) {
+      return session;
+    }
+
+    // access token not valid, try load new session
+    if (session != null && session!.canRefresh) {
+      final refreshToken = session!.refreshToken!;
+      final newSession = await loader(refreshToken);
+      if (newSession != null) {
+        // session get updated
+        await login(newSession);
+        return session;
+      }
+
+      if (newSession == null) {
+        await logout();
+      }
+    }
+    return null;
+  }
+
+  /// login new session
+  /// ```dart
+  /// await sessionProvider.login(
+  ///    session: Session(),
+  ///  );
+  /// ```
+  Future<void> login(Session newSession) async {
+    bool noPreviousSession = session == null;
+    session = newSession;
+    await session!.save();
+    if (noPreviousSession) {
+      await eventbus.broadcast(LoginEvent());
+    }
+    //  log.log('[app] login ${newSession.accessToken.value}');
+    notifyListeners();
+  }
+
+  /// logout session
+  /// ```dart
+  /// sessionProvider.logout();
+  /// ```
+  Future<void> logout() async {
+    bool hasSession = session != null;
+    if (hasSession) {
+//      log.log('[app] logout ${session!.userId}');
+    }
+    session?.accessToken.value = '';
+    session = null;
+    await Session.remove();
+    if (hasSession) {
+      await eventbus.broadcast(LogoutEvent());
+    }
+    notifyListeners();
+  }
+}
