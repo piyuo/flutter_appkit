77b5e89c2e6612567b2d3a99d384c1c2346d0da9
piyuo <piyuo.com@gmail.com>
2023-09-27 19:41:14 -0700

    Add mock auth lot of page need mock signin

---
+++ lib/testing/src/mock_auth.dart
@@ -0,0 +1,29 @@
+import 'package:libcli/auth/auth.dart' as auth;
+import 'package:libcli/net/net.dart' as net;
+
+void mockAuthResponse(auth.AuthService authService) {
+  authService.mockSender = (net.Object action, {net.Builder? builder}) async {
+    if (action is auth.VerifyEmailAction) {
+      return auth.SendPinResponse(result: auth.SendPinResponse_Result.RESULT_OK);
+    }
+
+    if (action is auth.LoginPinAction) {
+      return auth.LoginPinResponse(
+        result: auth.LoginPinResponse_Result.RESULT_OK,
+        access: auth.Access(
+          state: auth.Access_State.STATE_OK,
+          region: auth.Access_Region.REGION_UNSPECIFIED,
+          accessToken: 'fakeAccess',
+          accessExpire: DateTime.now().add(const Duration(seconds: 300)).timestamp,
+          refreshToken: 'fakeRefresh',
+          refreshExpire: DateTime.now().add(const Duration(days: 300)).timestamp,
+        ),
+      );
+    }
+
+    if (action is auth.ResendPinAction) {
+      return auth.SendPinResponse(result: auth.SendPinResponse_Result.RESULT_OK);
+    }
+    throw Exception('$action is not supported');
+  };
+}
--- lib/testing/testing.dart
+++ lib/testing/testing.dart
@@ -4,6 +4,7 @@ export 'src/redraw_provider.dart';
 export 'src/richtext.dart';
 export 'src/navigator.dart';
 export 'src/mock_app.dart';
+export 'src/mock_auth.dart';
 export 'src/example.dart';
 export 'src/is_test_mode.dart';
 export 'src/theme.dart';
