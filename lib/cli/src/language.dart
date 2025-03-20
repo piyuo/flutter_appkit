4d1bcde03f5dacb95d7ff4eb800e55d307849480
piyuo <piyuo.com@gmail.com>
2025-03-20 12:36:22 -0700

    feat: enhance Language class to include English name and improve locale handling

--- lib/cli/src/language.dart
+++ lib/cli/src/language.dart
@@ -3,18 +3,34 @@ import 'package:locale_names/locale_names.dart';
 
 /// A class that represents a language with its locale and name.
 class Language {
+  /// the locale of the language.
   final Locale locale;
+
+  /// the name of the language in its native form.
   final String name;
 
-  Language(this.locale, this.name);
+  /// the name of the language in English.
+  final String engName;
 
+  /// Creates a [Language] instance.
+  Language({required this.locale, required this.name, required this.engName});
+
+  /// Returns the display name of the language in the format "engName - name".
+  String get displayName => '$engName - $name';
+
+  /// Returns a list of [Language] instances from the given list of supported locales.
   static List<Language> fromSupportedLocales(List<Locale> supportedLocales) {
-    return supportedLocales.map((locale) {
+    final enLocale = Locale('en');
+    final result = supportedLocales.map((locale) {
       String name = locale.nativeDisplayLanguage;
       if (locale.nativeDisplayCountry.isNotEmpty) {
         name += ' (${locale.nativeDisplayCountry})';
       }
-      return Language(locale, name);
+
+      String engName = locale.displayLanguageIn(enLocale);
+      return Language(locale: locale, name: name, engName: engName);
     }).toList();
+    result.sort((a, b) => a.engName.compareTo(b.engName));
+    return result;
   }
 }
