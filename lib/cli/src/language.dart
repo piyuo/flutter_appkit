8ed536e88a4a8f6003e0853a263db682a314548c
piyuo <piyuo.com@gmail.com>
2025-03-27 13:30:09 -0700

    feat: add systemLocale getter to retrieve the system locale of the app

--- lib/cli/src/language.dart
+++ lib/cli/src/language.dart
@@ -43,6 +43,9 @@ Locale parseLocale(String localeString) {
 /// return the default locale of the app
 Locale? get defaultLocale => Intl.defaultLocale == null ? null : parseLocale(Intl.defaultLocale!);
 
+/// return the system locale of the app
+Locale? get systemLocale => parseLocale(Intl.systemLocale);
+
 /// return is using the system locale
 bool get isSystemLocale => Intl.defaultLocale == null || Intl.systemLocale == Intl.defaultLocale;
 
